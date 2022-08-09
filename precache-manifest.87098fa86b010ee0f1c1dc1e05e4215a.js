7242ab7e9b02f27dbb5cdb5cb7d176e0f0a10524
Parmpreet1 <Parmpreet1@github.com>
2022-08-10 00:58:50 +0530

    Updates

---
+++ precache-manifest.87098fa86b010ee0f1c1dc1e05e4215a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5da918304b4f0901fd77",
+    "url": "/My-todos-List-with-react-js/static/css/main.d9f7f76a.chunk.css"
+  },
+  {
+    "revision": "5da918304b4f0901fd77",
+    "url": "/My-todos-List-with-react-js/static/js/main.5da91830.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/My-todos-List-with-react-js/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "19c805cea6d66bfb350a",
+    "url": "/My-todos-List-with-react-js/static/js/2.19c805ce.chunk.js"
+  },
+  {
+    "revision": "375e6120bb1df7481435",
+    "url": "/My-todos-List-with-react-js/static/js/runtime~main.375e6120.js"
+  },
+  {
+    "revision": "748353bfc3adcbd231650f6b6fd3e7b1",
+    "url": "/My-todos-List-with-react-js/index.html"
+  }
+];
